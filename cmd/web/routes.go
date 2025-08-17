4e9fe52d8cc5ce8e93c680eeffd8c8c83f728f6a
Jaime Still <jpstill85@gmail.com>
2025-08-17 10:14:19 -0400

    chapter(6): complete

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -13,9 +17,11 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(
-		app.logRequest(
-			commonHeaders(mux),
-		),
+	standard := alice.New(
+		app.recoverPanic,
+		app.logRequest,
+		commonHeaders,
 	)
+
+	return standard.Then(mux)
 }
